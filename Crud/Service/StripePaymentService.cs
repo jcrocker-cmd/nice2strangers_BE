81cac36a470f99afce471aa65ca9a3fef71d5260
jcrocker-cmd <narbajajc@gmail.com>
2025-07-07 17:44:52 +0800

    added: Stripe integration

---
+++ Crud/Service/StripePaymentService.cs
@@ -0,0 +1,80 @@
+﻿using Crud.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+
+
+namespace Crud.Service
+{
+    public class StripePaymentService
+    {
+        private readonly IConfiguration _config;
+
+        public StripePaymentService(IConfiguration config)
+        {
+            _config = config;
+            StripeConfiguration.ApiKey = _config["Stripe:SecretKey"];
+        }
+
+        public Session CreateCheckoutSession(string successUrl, string cancelUrl)
+        {
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            Currency = "usd",
+                            UnitAmount = 5000, // 50.00 USD
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "My Product",
+                            },
+                        },
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = successUrl,
+                CancelUrl = cancelUrl,
+            };
+
+            var service = new SessionService();
+            return service.Create(options);
+        }
+
+
+        public Session CreateCheckout(List<ProductViewModel> products, string successUrl, string cancelUrl)
+        {
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = products.Select(p => new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        Currency = "usd",
+                        UnitAmount = p.PriceInCents,
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = p.Name
+                        }
+                    },
+                    Quantity = 1
+                }).ToList(),
+                Mode = "payment",
+                SuccessUrl = successUrl,
+                CancelUrl = cancelUrl,
+            };
+
+            var service = new SessionService();
+            return service.Create(options);
+        }
+
+    }
+
+
+}

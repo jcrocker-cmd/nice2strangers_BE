cf875a8d2a39b77cab9909a7697b3289bc76d2c7
jcrocker-cmd <narbajajc@gmail.com>
2025-09-23 17:01:36 +0800

    added forgot and reset password

---
+++ Crud/ViewModel/Auth/ForgotPasswordViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Crud.ViewModel.Auth
+{
+    public class ForgotPasswordViewModel
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ Crud/ViewModel/Auth/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Crud.ViewModel.Auth
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

20e59457d2f90366125d2c018de2b9c1dab32fa9
jcrocker-cmd <narbajajc@gmail.com>
2025-09-25 15:32:30 +0800

    modify docker donfig

---
+++ Crud/Contracts/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Crud.ViewModel.Auth;
+
+namespace Crud.Contracts
+{
+    public interface IAuthService
+    {
+        Task<(bool Succeeded, IEnumerable<string> Errors)> RegisterUserAsync(RegisterViewModel model);
+        Task<(bool Succeeded, IEnumerable<string> Errors)> RegisterAdminAsync(RegisterViewModel model);
+        Task<(string Token, string Email, string Role, string FirstName, string LastName)?> LoginAsync(LoginViewModel model);
+        Task<bool> ForgotPasswordAsync(ForgotPasswordViewModel model);
+        Task<(bool Succeeded, IEnumerable<string> Errors)> ResetPasswordAsync(ResetPasswordViewModel model);
+    }
+}

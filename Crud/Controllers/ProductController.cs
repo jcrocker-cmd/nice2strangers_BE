7703191b51b315435974230753e8548cd9c14359
John Christian B. Narbaja <56188640+jcrocker-cmd@users.noreply.github.com>
2025-09-26 08:46:25 +0800

    Update ProductController.cs

--- Crud/Controllers/ProductController.cs
+++ Crud/Controllers/ProductController.cs
@@ -7,7 +7,6 @@ namespace Crud.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class ProductController : ControllerBase
     {
         private readonly IProductService _productService;
